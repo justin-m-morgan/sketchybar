c399cf40d3895f122e114bd7630eb21cb6da23f7
Justin Morgan <justin.morgan@hey.com>
2025-11-09 17:07:05 -0800

    Make media plugin poll on interval

--- items/media.sh
+++ items/media.sh
@@ -8,4 +8,5 @@ sketchybar --add item media e \
                        icon.color=$WHITE   \
                        background.drawing=on \
                        script="$PLUGIN_DIR/media.sh" \
+                       update_freq=5 \
            --subscribe media media_change
